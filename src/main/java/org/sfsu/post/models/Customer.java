8f4d62c7238b46f690dfecbd39108c771b3e1ff1
Steve <steveytan@gmail.com>
2017-02-07 15:03:26 -0800

    added simple customer class

--- src/main/java/org/sfsu/post/models/Customer.java
+++ src/main/java/org/sfsu/post/models/Customer.java
@@ -2,6 +2,34 @@ package org.sfsu.post.models;
 
 /**
  * Created by rajanishivarajmaski1 on 2/6/17.
+ * 
  */
 public class Customer {
+    
+    
+    private String name;
+    
+    
+    /* Default and main constructors */
+    public Customer(){
+        this.name = "";
+    }
+    
+    public Customer(String name){
+        this.name = name;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+   
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    } 
 }
