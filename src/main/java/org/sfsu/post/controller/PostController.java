d524161b1e52e98d9b4e2b8ee0a9fce41e1585d7
SteveYTan <SteveYTan@gmail.com>
2017-02-09 07:06:16 -0800

    Update PostController.java

--- src/main/java/org/sfsu/post/controller/PostController.java
+++ src/main/java/org/sfsu/post/controller/PostController.java
@@ -1,74 +1,17 @@
 package org.sfsu.post.controller;
 
-import java.util.Scanner;
-import org.sfsu.post.models.Customer;
-import org.sfsu.post.models.Payment;
-
 
 /**
- *
- * @author Steve Tan 2/8/16
+ * Created by rajanishivarajmaski1 on 2/6/17.
  */
-public class FrontEndController {
+public class PostController {
     
     
-    private Transaction trans = new Transaction();
-    
-    
-    public FrontEndController(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" Welcome to S-Mart. \n Please enter your name ");
+    public static void main(String[] args) {
         
-        //create customer object;
-        String name = sc.nextLine();
-        Customer cust = new Customer(name);
-        trans.setCustomer(cust);
+       FrontEndController frontend = new FrontEndController();
+       Transaction trans = frontend.getTransaction();
         
-        Boolean repeat = true;
-        
-        
-        //loop ask user for a code or exit        
-        while(repeat){
-            
-            if (trans.numTransItems == 0)
-            {
-                System.out.println(" Welcome " + name + "! \n Please enter the UPC to get started");
-            } else {
-                System.out.println(" Please enter the UPC Code or type 'exit' to finish scanning");
-            }
-            
-            // exit clause when user types quit
-            String code = sc.nextLine();
-            if(code.equals("exit")) {
-                repeat = false;
-                break;
-            }
-            
-            //create a new transaction item
-            TransactionItem item = new TransactionItem(code); 
-            
-            //Ask user for quantity and set it
-            System.out.println(" How many would you like to buy?");
-            item.setQuantity(sc.nextInt());
-            sc.nextLine();         
-            
-            //add item to trans
-            trans.addTransactionItem(item);        
-        }
-        
-        System.out.println("How would you like to pay? Type 'cash' or 'credit'");
-        String type = sc.nextLine();
-        
-        Payment pay = new Payment(type, trans.getTotal());
-        trans.setPayment(pay);
-
         
     }
-    
-    
-    public Transaction getTransaction(){
-        return trans;
-    }
-    
-    
 }
