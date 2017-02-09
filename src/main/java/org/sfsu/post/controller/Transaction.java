8939f741c3202c8dbbec4d6aec5d37a97de97b80
PotatoBuggy <spencer.dennis21@gmail.com>
2017-02-08 21:30:03 -0800

    Update Transaction.java

--- src/main/java/org/sfsu/post/controller/Transaction.java
+++ src/main/java/org/sfsu/post/controller/Transaction.java
@@ -3,21 +3,21 @@ public class Transaction {
    public TransactionItem[] transItems;
    public int size= 100;
    public int numTransItems;
-   // private Payment payment;
-   // private Customer customer;
+   public Payment payment;
+   public Customer customer;
    
    //Constructor
    public Transaction(){
 	   this.transItems= new TransactionItem[size];
 	   this.numTransItems=0;
-	   //this.payment= new Payment(null, 0.0);
-	   //this.customer=new Customer(null);
+	   this.payment= new Payment(null, 0.0);
+	   this.customer=new Customer(null);
    }
    
    //Set Customer Object
-   /*public void setCustomer(Customer customer){
+   public void setCustomer(Customer customer){
 	   this.customer= customer;
-   }*/
+   }
    
    //Add items to TransactionItem's object array
    public void addTransactionItem(TransactionItem items){
@@ -25,17 +25,17 @@ public class Transaction {
 	   this.numTransItems++;
    }
    
-   //???
+   //Not Needed for Now
    /*public boolean notFull(){
 	   return true;
    }*/
    
    //Set Payment Object
-   /*public void setPayment(Payment pay){
+   public void setPayment(Payment pay){
 	   payment=pay;
-   }*/
+   }
    
-   //???
+   //Not Needed for Now
    /*public void toString(){
 	   
    }*/
