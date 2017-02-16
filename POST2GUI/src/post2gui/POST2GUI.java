02f075d252b4a3a490f47ef5f2abd84d4bb6ad0c
MYAT MAUNG <MYAT MAUNG>
2017-02-15 17:44:32 -0800

    Updated: GUI 90% working. Can be used to test REST.

---
+++ POST2GUI/src/post2gui/POST2GUI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package post2gui;
+
+/**
+ *
+ * @author MyatMinMaung
+ */
+public class POST2GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        postGUI gui= new postGUI();
+        gui.setVisible(true);
+    }
+    
+}
